a6f821b87b1aafecc80a7d45b1c4aed81b0e92f5
chuka <chukachuka993@gmail.com>
2021-02-03 17:56:51 +0800

    togloomoo hiij duuslaa

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+// togloom duussan eseh hadgalah huvisagchj
+var isGameOver;
 //togloomiiin buh gazar global huvisagch zarlah
+
 var activePlayer, score, rounScore;
 
 // shoonii zurgiig uzuuleh
@@ -7,6 +10,9 @@ var diceDom = document.querySelector(".dice");
 // togloom ehluuleh
 initGame();
 function initGame() {
+  //  togloom eheellle gdg tolow oruulna
+  isGameOver = false;
+
   // toglogchiin eeljiig hadgalah huvisagch
 
   activePlayer = 0;
@@ -43,45 +49,57 @@ function initGame() {
 
 //  shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  //    1 - 6 hurtel sanamsargui too gargaj avna
-  var diceNumber = Math.floor(Math.random() * 6) + 1;
-  //   sshoonii zurg web deer gargaj irne
-  diceDom.style.display = "block";
-  // buusan sanamsargui toond hargalzah shoog web dr gargaj irne
-  diceDom.src = "dice-" + diceNumber + ".png";
-  //   buusan too n 1  ees oor bol  idevehtei toglogchiin eeljiin onoog nemegduulne
-  if (diceNumber !== 1) {
-    //   1 ees ylaagtai too buulga
-    rounScore = rounScore + diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = rounScore;
+  if (isGameOver !== true) {
+    //    1 - 6 hurtel sanamsargui too gargaj avna
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    //   sshoonii zurg web deer gargaj irne
+    diceDom.style.display = "block";
+    // buusan sanamsargui toond hargalzah shoog web dr gargaj irne
+    diceDom.src = "dice-" + diceNumber + ".png";
+    //   buusan too n 1  ees oor bol  idevehtei toglogchiin eeljiin onoog nemegduulne
+    if (diceNumber !== 1) {
+      //   1 ees ylaagtai too buulga
+      rounScore = rounScore + diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = rounScore;
+    } else {
+      //   1 buusan tul toglogchiin eeljiig solino
+      // ene toglogchiin eeljinee tsugluulsan onoog 0 bolgoloo
+      switchToNextPlayer();
+    }
   } else {
-    //   1 buusan tul toglogchiin eeljiig solino
-    // ene toglogchiin eeljinee tsugluulsan onoog 0 bolgoloo
-    switchToNextPlayer();
+    alert("togloom duussan bna new game towch darj shineer ehlene uu ");
   }
 });
 
 // hold towchnii listener
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  // ug toglogchiin tsugluulsan onoog global onoon derr nemj ogno
-  score[activePlayer] = score[activePlayer] + rounScore;
+  if (isGameOver) {
+    // ug toglogchiin tsugluulsan onoog global onoon derr nemj ogno
+    score[activePlayer] = score[activePlayer] + rounScore;
 
-  //   web re toog n oorchlono
-  document.getElementById("score-" + activePlayer).textContent =
-    score[activePlayer];
+    //   web re toog n oorchlono
+    document.getElementById("score-" + activePlayer).textContent =
+      score[activePlayer];
 
-  // ug toglogch hojison eseh shalgah
-  if (score[activePlayer] >= 100) {
-    //   ylagch gsen ttext web dr gargah
-    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
+    // ug toglogch hojison eseh shalgah
+    if (score[activePlayer] >= 10) {
+      //   tolgoomiig duussan tolowt oruulna
+      isGameOver = true;
+      //   ylagch gsen ttext web dr gargah
+      document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+    } else {
+      switchToNextPlayer();
+    }
   } else {
-    switchToNextPlayer();
+    alert("togloom duussan bna new game towch darj shineer ehlene uu ");
   }
 });
 
